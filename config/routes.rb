83a221014c009b2bb7e2267bd2f282f4682e6ec1
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 19:05:10 +0600

    Setup the route and controller for food

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index]
+  root "foods#index"
 end
