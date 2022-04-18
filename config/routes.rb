cb9bbaf84d0fb0fe7fef6ebd51f03fac81d42a88
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 19:09:22 +0600

    Added the remaining routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   resources :foods, only: [:index]
+  resources :recipes, only: [:index, :show]
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
   root "foods#index"
 end
