ac084aaed6945303e6485b139fbdc6741613bf17
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 21:45:09 +0600

    Update Recipes Controller to solve N+1 issue

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,7 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.includes(:foods).find(params[:id])
   end
 
   def new
