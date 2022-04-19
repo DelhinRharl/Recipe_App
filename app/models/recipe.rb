ad43007285101914e08785101488b6bd905fba3e
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 17:53:48 +0600

    Added name validation for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 250 }
 end
