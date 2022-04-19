a7da1f9b15242563c285513e1d1ba0ed2b3073f3
DelhinRharl <delhinrharl@gmail.com>
2022-04-20 00:56:24 +0100

    Fix linter

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,9 +2,9 @@ class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.where(public: true).each do |recipe|
       {
-      food_items: recipe.foods.count,
-      total_price: recipe.foods.map.sum(:price)
-    }
+        food_items: recipe.foods.count,
+        total_price: recipe.foods.map.sum(:price)
+      }
     end
   end
 end
