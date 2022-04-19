3dbabfe4f46930eacb90137e6dd5273c83c578d3
DelhinRharl <delhinrharl@gmail.com>
2022-04-19 15:56:00 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   
   resources :foods, only: [:index, :create, :new]
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get '/foods', to: 'foods#index', as: 'user_root'
-  # Defines the root path route ("/")
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
