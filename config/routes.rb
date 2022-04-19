d9d37e8923c194f7e19adcdbc39aca20454b873b
DelhinRharl <delhinrharl@gmail.com>
2022-04-19 13:13:40 +0100

    Add create and new to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  resources :foods, only: [:index]
+  
+  resources :foods, only: [:index, :create, :new]
   resources :recipes, only: [:index, :show]
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
