192788b80feb0332ad2877eb9b11b659cdffad4a
DelhinRharl <delhinrharl@gmail.com>
2022-04-20 00:52:24 +0100

    Add funtion to the contoller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,10 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true)
-   end
+    @recipes = Recipe.where(public: true).each do |recipe|
+      {
+      food_items: recipe.foods.count,
+      total_price: recipe.foods.map.sum(:price)
+    }
+    end
+  end
 end
