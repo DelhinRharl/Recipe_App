baf7a054956d0cdb58d17a6e190f91d720b06668
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-20 01:45:52 +0600

    Fix routes for recipe foods

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   get '/foods', to: 'foods#index', as: 'user'
   # Defines the root path route ("/")
   resources :foods, only: [:index, :show, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
-  resources :recipe_foods, only: [:new, :create, :destroy]
   root "foods#index"
 end
