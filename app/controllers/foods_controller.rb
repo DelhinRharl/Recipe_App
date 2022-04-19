45c3ab61546caef344088ccca9c5f7c239bf234e
DelhinRharl <delhinrharl@gmail.com>
2022-04-19 13:10:41 +0100

    Add CRUD to the controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,31 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to new_foods_path, notice: 'You have added a new food!'
+    else
+      redirect_to foods_path, notice: "Food not saved!"
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy!
+    flash[:notice] = 'You have deleted the food!'
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
