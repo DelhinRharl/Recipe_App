5842a258337424afc1c98eba7ddaae2e38c33436
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 18:17:36 +0600

    Update routes and recipe#show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/foods', to: 'foods#index', as: 'user_root'
   # Defines the root path route ("/")
-  resources :foods, only: [:index]
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
