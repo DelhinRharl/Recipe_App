c87639fbb61f558706f21ea5516b49f595b36929
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 14:44:21 +0600

    Style recipes#index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,17 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    redirect_to public_recipes_path unless current_user
 
-  def show; end
+    @recipes = Recipe.all
+  end
+
+  def show 
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy!
+    redirect_to recipes_path, notice: "Recipe #{recipe.name} deleted successfully!"
+  end
 end
