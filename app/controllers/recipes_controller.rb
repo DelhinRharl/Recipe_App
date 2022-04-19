b2e83fe2b60f08e6e100f664e1525dcb825ec9ba
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 16:01:22 +0600

    Add new recipe page and add functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,10 +8,32 @@ class RecipesController < ApplicationController
   def show 
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    redirect_to public_recipes_path unless current_user
+
+    recipe = Recipe.new(recipe_params)
+
+    if recipe.save
+      redirect_to recipe_path(recipe.id), notice: 'recipe was successfully created!'
+    else
+      redirect_to new_recipe_path, notice: 'recipe could not be created!'
+    end
+  end
   
   def destroy
     recipe = Recipe.find(params[:id])
     recipe.destroy!
     redirect_to recipes_path, notice: "Recipe #{recipe.name} deleted successfully!"
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :user_id, :preparation_time, :cooking_time, :description, :public)
   end
 end
