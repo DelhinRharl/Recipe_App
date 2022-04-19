b2b3f5a66d8b466734e1f63f39acfa7cc6f1b3bf
DelhinRharl <delhinrharl@gmail.com>
2022-04-19 20:57:20 +0100

    Modify Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :foods, only: [:index, :create, :new]
+  resources :foods, only: [:index, :create, :new, :destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
