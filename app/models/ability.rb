7a201870ee423d49c69e107e7d8fe1748684a430
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-19 16:55:57 +0600

    Update abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+    user ||= User.new # guest user (not logged in)
+  
+    can :manage, Recipe, user_id: user.id
+    can :manage, Food, user_id: user.id
+    can :read, :all
+      
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
