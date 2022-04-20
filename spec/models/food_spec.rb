c5a6f4e4c6877ab68b7ee04c39a6026d7b0a170e
DelhinRharl <delhinrharl@gmail.com>
2022-04-21 00:16:28 +0100

    Add foods controller tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Food model' do
+    @user = User.create(name: 'Affax', email: 'toast@example.com', password: '123456')
+    subject { Food.new(user_id: @user, name: 'Tomato', measurement_unit: 'g', price: 12) }
+    before { subject.save }
+
+    it 'checks if name is not blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check that the measurement_unit is not blank' do
+      subject.measurement_unit = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'checks that the price is not empty' do
+      subject.price = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
